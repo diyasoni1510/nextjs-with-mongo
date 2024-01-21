822ade83f753786560f660fa8228e661cf3b53a1
diya <sonidiya126@gmail.com>
2024-01-21 15:15:41 +0530

    remove props from profileheader

--- src/app/profile/profileheader/page.jsx
+++ src/app/profile/profileheader/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 import { FaHome } from "react-icons/fa";
@@ -43,24 +43,24 @@ const headerItems = [
   },
 ];
 
-const ProfileHeader = ({props}) => {
-  const router = useRouter()
+const ProfileHeader = () => {
+  const router = useRouter();
   const setUserLogout = () => {
-    localStorage.removeItem("userToken")
-    router.push("/")
-  }
+    localStorage.removeItem("userToken");
+    router.push("/");
+  };
   return (
     <div className="bg-pink-50 shadow flex justify-center items-center fixed top-0 w-full z-50">
       <div className="container">
         <div className="flex items-center justify-between py-4">
           <div className="logo text-2xl tracking-widest pl-2">GupShup</div>
           <div className="header-icons space-x-4 items-center justify-center flex md:hidden pr-2">
-                <Link href="/" className="text-2xl hover:text-pink-300">
-                  <FaRegHeart/>
-                </Link>
-                <Link href="/MessagePage" className="text-2xl hover:text-pink-300">
-                  <AiFillMessage/>
-                </Link>
+            <Link href="/" className="text-2xl hover:text-pink-300">
+              <FaRegHeart />
+            </Link>
+            <Link href="/MessagePage" className="text-2xl hover:text-pink-300">
+              <AiFillMessage />
+            </Link>
           </div>
         </div>
       </div>
