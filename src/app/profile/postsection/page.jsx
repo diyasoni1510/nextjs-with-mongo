f714af8ac939ae09587f5aea46fd0ab909e0d3b7
divyanjali soni <divyanjali@360adaptive.com>
2024-02-01 17:36:56 +0530

    commit

--- src/app/profile/postsection/page.jsx
+++ src/app/profile/postsection/page.jsx
@@ -40,7 +40,7 @@ const PostSection = () => {
       <div className="h-[600px] flex justify-center items-center">
         <svg
           aria-hidden="true"
-          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-700"
+          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
