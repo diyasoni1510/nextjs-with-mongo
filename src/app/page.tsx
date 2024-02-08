d4a75da6617cfa38c16d542b60af9abb4262f982
divyanjali soni <divyanjali@360adaptive.com>
2024-02-08 16:46:16 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Tabs = () => {
   const [openTab, setOpenTab] = React.useState(1);
   return (
     <>
-      <div className="flex flex-wrap h-[600px]  px-2 ">
+      <div className="flex flex-wrap h-[500px]  px-2 ">
         <div className="w-full">
           <ul
             className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
