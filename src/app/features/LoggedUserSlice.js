8f417fdd594ed6a1131b14de834cb20d9a93ac63
divyanjali soni <divyanjali@360adaptive.com>
2024-03-07 12:44:13 +0530

    create store and slice

--- src/app/features/LoggedUserSlice.js
+++ src/app/features/LoggedUserSlice.js
@@ -1,1 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    LoggedUser:{}
+}
+
+export const LoggedUserSlice = createSlice({
+    name:"LoggedUser",
+    initialState,
+    reducers:{
+        setLoggedUser: () => {}
+    }
+})
+
+export const {setLoggedUser} = LoggedUserSlice.actions
+
+export default LoggedUserSlice.reducer
